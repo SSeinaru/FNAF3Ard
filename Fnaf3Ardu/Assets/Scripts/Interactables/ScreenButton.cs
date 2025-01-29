a27b1a2be70fa2f5b2831d0994a264514aa0ccf6
Seinaru <34807@ma-web.nl>
2025-01-29 12:24:08 +0100

    Quick Script Fix:

--- Fnaf3Ardu/Assets/Scripts/Interactables/ScreenButton.cs
+++ Fnaf3Ardu/Assets/Scripts/Interactables/ScreenButton.cs
@@ -9,7 +9,8 @@ public class ScreenButton : MonoBehaviour
     [SerializeField] private string buttonID;
 
     [Header("Visual Feedback")]
-    [SerializeField] private Animator staticAnimation;
+    [SerializeField] private Animator mainAnimation;
+    [SerializeField] private Animator ventAnimation;
 
     private StaticScreenController _screenController;
     private Dictionary<string, System.Action> _mainButtonActions;
@@ -26,31 +27,30 @@ public class ScreenButton : MonoBehaviour
     {
         _mainButtonActions = new Dictionary<string, System.Action>
         {
-            { "CAM01", () => _screenController.SetCameraPosition(0) },
-            { "CAM02", () => _screenController.SetCameraPosition(1) },
-            { "CAM03", () => _screenController.SetCameraPosition(2) },
-            { "CAM04", () => _screenController.SetCameraPosition(3) },
-            { "CAM05", () => _screenController.SetCameraPosition(4) },
-            { "CAM06", () => _screenController.SetCameraPosition(5) },
-            { "CAM07", () => _screenController.SetCameraPosition(6) },
-            { "CAM08", () => _screenController.SetCameraPosition(7) },
-            { "CAM09", () => _screenController.SetCameraPosition(8) },
-            { "CAM10", () => _screenController.SetCameraPosition(9) },
+            { "CAM01", () => { _screenController.SetCameraPosition(0); ButtonPressEffect(0);}},
+            { "CAM02", () => { _screenController.SetCameraPosition(1); ButtonPressEffect(1);}},
+            { "CAM03", () => { _screenController.SetCameraPosition(2); ButtonPressEffect(2);}},
+            { "CAM04", () => { _screenController.SetCameraPosition(3); ButtonPressEffect(3);}},
+            { "CAM05", () => { _screenController.SetCameraPosition(4); ButtonPressEffect(4);}},
+            { "CAM06", () => { _screenController.SetCameraPosition(5); ButtonPressEffect(5);}},
+            { "CAM07", () => { _screenController.SetCameraPosition(6); ButtonPressEffect(6);}},
+            { "CAM08", () => { _screenController.SetCameraPosition(7); ButtonPressEffect(7);}},
+            { "CAM09", () => { _screenController.SetCameraPosition(8); ButtonPressEffect(8);}},
+            { "CAM10", () => { _screenController.SetCameraPosition(9); ButtonPressEffect(9);}},
         };
         _ventButtonActions = new Dictionary<string, System.Action>
         {
-            { "CAM11", () => _screenController.SetCameraPosition(10) },
-            { "CAM12", () => _screenController.SetCameraPosition(11) },
-            { "CAM13", () => _screenController.SetCameraPosition(12) },
-            { "CAM14", () => _screenController.SetCameraPosition(13) },
-            { "CAM15", () => _screenController.SetCameraPosition(14) },
+            { "CAM11", () => { _screenController.SetCameraPosition(10); ButtonPressEffect(10); }},
+            { "CAM12", () => { _screenController.SetCameraPosition(11); ButtonPressEffect(11); }},
+            { "CAM13", () => { _screenController.SetCameraPosition(12); ButtonPressEffect(12); }},
+            { "CAM14", () => { _screenController.SetCameraPosition(13); ButtonPressEffect(13); }},
+            { "CAM15", () => { _screenController.SetCameraPosition(14); ButtonPressEffect(14); }},
 
         };
     }
 
     public void OnButtonPress()
     {
-        ButtonPressEffect();
         
         if (_mainButtonActions.ContainsKey(buttonID))
         {
@@ -70,8 +70,13 @@ public class ScreenButton : MonoBehaviour
         }
     }
 
-    private void ButtonPressEffect()
+    private void ButtonPressEffect(int index)
     {
-        staticAnimation.SetTrigger("CamTrans");
+        if (index <= 9)
+            mainAnimation.SetTrigger("CamTrans");
+
+        else
+            ventAnimation.SetTrigger("VentCam");
     }
+}
 }
--- Fnaf3Ardu/Assets/Scripts/Interactables/StaticScreenController.cs
+++ Fnaf3Ardu/Assets/Scripts/Interactables/StaticScreenController.cs
@@ -7,8 +7,10 @@ public class StaticScreenController : MonoBehaviour
 {
     [Header("Screen Settings")]
     [SerializeField] private Transform screenCamera;
-
     [SerializeField] private Transform ventCamera;
+
+    [SerializeField] private Animator mainAnimation;
+    [SerializeField] private Animator ventAnimation;
 
     [Header("Camera Positions")]
     [SerializeField] private List<Transform> cameraPositions = new List<Transform>();
@@ -17,12 +19,16 @@ public class StaticScreenController : MonoBehaviour
     [SerializeField] private LayerMask buttonLayer;
     [SerializeField] private float maxRaycastDistance = 3f;
 
+    private Dictionary<string, System.Action> _mainButtonActions;
+    private Dictionary<string, System.Action> _ventButtonActions;
+
     private Camera _mainCamera;
     internal object _serialPort;
 
     void Start()
     {
         _mainCamera = Camera.main;
+        SetupButtonActions();
     }
 
     void Update()
@@ -31,6 +37,33 @@ public class StaticScreenController : MonoBehaviour
         {
             HandleButtonPress();
         }
+    }
+    private void SetupButtonActions()
+    {
+        _mainButtonActions = new Dictionary<string, System.Action>
+        {
+            {"CAM01", () => { SetCameraPosition(0); ButtonPressEffect(0); }},
+            {"CAM02", () => { SetCameraPosition(1); ButtonPressEffect(1); }},
+            {"CAM03", () => { SetCameraPosition(2); ButtonPressEffect(2); }},
+            {"CAM04", () => { SetCameraPosition(3); ButtonPressEffect(3); }},
+            {"CAM05", () => { SetCameraPosition(4); ButtonPressEffect(4); }},
+            {"CAM06", () => { SetCameraPosition(5); ButtonPressEffect(5); }},
+            {"CAM07", () => { SetCameraPosition(6); ButtonPressEffect(6); }},
+            {"CAM08", () => { SetCameraPosition(7); ButtonPressEffect(7); }},
+            {"CAM09", () => { SetCameraPosition(8); ButtonPressEffect(8); }},
+            {"CAM10", () => { SetCameraPosition(9); ButtonPressEffect(9); }}
+        };
+
+        _ventButtonActions = new Dictionary<string, System.Action>
+        {
+            {"CAM11", () => { SetCameraPosition(10); ButtonPressEffect(10); }},
+            {"CAM12", () => { SetCameraPosition(11); ButtonPressEffect(11); }},
+            {"CAM13", () => { SetCameraPosition(12); ButtonPressEffect(12); }},
+            {"CAM14", () => { SetCameraPosition(13); ButtonPressEffect(13); }},
+            {"CAM15", () => { SetCameraPosition(14); ButtonPressEffect(14); }},
+            {"Lure", () => { ButtonPressEffect(15); }},
+            {"LockDown", () => { ButtonPressEffect(16); }}
+        };
     }
 
     void HandleButtonPress()
@@ -63,5 +96,14 @@ public class StaticScreenController : MonoBehaviour
             ventCamera.rotation = cameraPositions[index].rotation;
             Debug.Log(cameraPositions[index].position);
         }
+    }
+
+    private void ButtonPressEffect(int index)
+    {
+        if (index <= 9)
+            mainAnimation.SetTrigger("CamTrans");
+
+        else
+            ventAnimation.SetTrigger("VentCam");
     }
 }
